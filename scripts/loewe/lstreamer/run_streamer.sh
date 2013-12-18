91a698bc2a48ce02f8e160360e516de63c1f47af
user <user@user-To-Be-Filled-By-O-E-M.(none)>
2013-12-18 13:55:49 +0400

    DTG streams

--- scripts/loewe/lstreamer/run_streamer.sh
+++ scripts/loewe/lstreamer/run_streamer.sh
@@ -3,5 +3,13 @@
 #done;
 killall lstreamer
 
-/var/www/_scripts/player/lstreamer --configfile /home/user/vkuchuk/AltInst1.ini& 
-/var/www/_scripts/player/lstreamer --configfile /home/user/vkuchuk/AltInst2.ini& 
+LSTREAMER=/var/www/_scripts/player/lstreamer
+CONFIG_PATH=/home/user/vkuchuk/algo/scripts/loewe/lstreamer
+#CONFIG_FILE=AltInst1.ini
+#CONFIG_FILE=EITpf.ini
+CONFIG_FILE=Si26a.ini
+
+#/var/www/_scripts/player/lstreamer --configfile /home/user/vkuchuk/AltInst1.ini& 
+#/var/www/_scripts/player/lstreamer --configfile /home/user/vkuchuk/AltInst2.ini& 
+$LSTREAMER --configfile $CONFIG_PATH/$CONFIG_FILE
+
